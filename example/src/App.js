762dce4e947920b0bd52444c7e45dc89e611477a
browniu <niuniusoga@qq.com>
2019-06-25 19:50:33 +0800

    +

--- example/src/App.js
+++ example/src/App.js
@@ -4,10 +4,10 @@ import Krpano from 'react-krpano'
 
 export default class App extends Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
       popState: false
-    }
+    };
     this.hooks = {
       pop: (e) => this.pop(e)
     }
@@ -24,37 +24,22 @@ export default class App extends Component {
             {this.state.popState === 1 && <div className={'popup_main'}>
               <div className={'popup_button'} onClick={() => {
                 window.location = 'https://jx3.xoyo.com/'
-              }}/>
+              }} />
               <div className={'popup_close'} onClick={() => {
                 this.setState({popState: false})
-              }}/>
+              }} />
             </div>}
           </div>}
         </div>
-        <Krpano xml='krpano/tour.xml' hooks={this.hooks} mounted={this.mounted} loading={{info: '资源配置中'}} groy={true} dev={false}/>
+        <Krpano xml='krpano/tour.xml' hooks={this.hooks} mounted={this.mounted} loading={{info: '资源配置中'}} groy={true} dev={false} />
       </div>
     )
   }
 
-  componentDidMount() {
-    this.preload()
-  }
-
   pop(e) {
-    console.log('test function from user', e)
+    console.log('test function from user', e);
     this.setState({
       popState: e
-    })
-  }
-
-  preload() {
-    let list = [
-      'http://jx3.xoyo.com/zt/2019/06/17/pop/big.png',
-      'https://jx3.xoyo.com/zt/2019/06/17/operate/assets/images/book.dfe45355.png'
-    ]
-    list.map(e => {
-      let dom = document.createElement('img')
-      dom.src = e
     })
   }
 
