82ffb6c272786fc0b13449a74b0e0451a946dd23
browniu <niuniusoga@qq.com>
2019-06-26 14:00:01 +0800

    +

--- example/src/App.js
+++ example/src/App.js
@@ -1,15 +1,17 @@
 import React, {Component} from 'react'
-
 import Krpano from 'react-krpano'
 
 export default class App extends Component {
   constructor(props) {
-    super(props);
+    super(props)
     this.state = {
-      popState: false
-    };
+      popState: false,
+      eyes: false,
+      eyeMode: false
+    }
     this.hooks = {
-      pop: (e) => this.pop(e)
+      pop: (e) => this.pop(e),
+      eyeMode: () => { this.setState({eyes: true}) }
     }
   }
 
@@ -17,6 +19,7 @@ export default class App extends Component {
     return (
       <div>
         <div className={'panel'}>
+          {this.state.eyes && <button className={'eyes'} onClick={() => this.eyeModeSwitch()}>心眼模式</button>}
           {this.state.popState &&
           <div className={['popup', this.state.popState === 2 ? 'popup_second' : ''].join(' ')} onClick={() => {
             if (this.state.popState === 2) this.setState({popState: false})
@@ -37,17 +40,28 @@ export default class App extends Component {
   }
 
   pop(e) {
-    console.log('test function from user', e);
     this.setState({
       popState: e
     })
   }
 
-  next() {
-    window.krpano.call('loadscene(scene_test2,null,MERGE,BLEND(1.0, easeInCubic))')
+  eyeModeSwitch() {
+    let vl = window.krpano.get('view.vlookat')
+    let hl = window.krpano.get('view.hlookat')
+    if (this.state.eyeMode) window.krpano.call('loadscene(scene_inside,null,MERGE,BLEND(1.0, easeInCubic));')
+    else window.krpano.call('loadscene(scene_inside_eye,null,MERGE,BLEND(1.0, easeInCubic));')
+    setTimeout(() => {
+      window.krpano.set('view.hlookat', hl)
+      window.krpano.set('view.vlookat', vl)
+    }, 50)
+    this.setState({eyeMode: !this.state.eyeMode})
   }
 
-  mounted() {
+  next = () => {
+    window.krpano.call('loadscene(scene_test2,null,MERGE,BLEND(1.0, easeInCubic))')
+  };
+
+  mounted = () => {
     console.log('krpano is ready')
-  }
+  };
 }
